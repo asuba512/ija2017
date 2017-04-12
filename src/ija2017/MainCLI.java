5a510e453ebd9150a261f0d2d9d3bb5c23630cf9
Adam Suba <asuba512@gmail.com>
2017-04-12 12:53:09 +0200

    Improved CLI version by 250%

--- src/ija2017/MainCLI.java
+++ src/ija2017/MainCLI.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 public class MainCLI {
 
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         int opt;
         int tmp, tmp2, tmp3;
         Game gm1 = new Game();
         String menu = "Options:\n\t0 Print game\n\t-1 Undo\n\t1 Turn Next Card\n\t2 Move SourcePile to FoundationPile\n\t" +
                 "3 Move SourcePile to CardStack\n\t4 Move FoundationPile to CardStack\n\t5 Move CardStack to CardStack\n\t" +
-                "6 Move CardStack to FoundationPile\n\t-2 End";
+                "6 Move CardStack to FoundationPile\n\t7 Move FoundationPile to FoundationPile\n\t-2 End";
         System.out.println(menu);
         while((opt = scan.nextInt()) != -2){
             switch (opt){
@@ -27,56 +27,45 @@ public class MainCLI {
                     gm1.turnCard();
                     break;
                 case 2:
-                    System.out.println("FoundationPile? 1-4 ");
-                    if(gm1.canMoveToFoundation(tmp = scan.nextInt()-1))
+                    if((tmp = getFoundationIndex()) >= 0 && gm1.canMoveToFoundation(tmp))
                         gm1.moveToFoundation(tmp);
                     else
                         System.out.println(false);
                     break;
                 case 3:
-                    System.out.println("CardStack? 1-7 ");
-                    if(gm1.canMoveToStack((tmp = scan.nextInt()-1)))
+                    if((tmp = getStackIndex()) >= 0 && gm1.canMoveToStack(tmp))
                         gm1.moveToStack(tmp);
                     else
                         System.out.println(false);
                     break;
                 case 4:
-                    System.out.println("FoundationPile? 1-4 ");
-                    tmp = scan.nextInt() - 1;
-                    System.out.println("CardStack? 1-7 ");
-                    if(gm1.canMoveToStack((tmp2 = scan.nextInt() - 1), tmp))
+                    tmp = getFoundationIndex();
+                    if(tmp >= 0 && (tmp2 = getStackIndex()) >= 0 && gm1.canMoveToStack(tmp2, tmp))
                         gm1.moveToStack(tmp2, tmp);
                     else
                         System.out.println(false);
                     break;
                 case 5:
-                    System.out.println("CardStack? 1-7 ");
-                    tmp = scan.nextInt() - 1;
-                    System.out.println("Index (from top, starting with 1)? ");
-                    tmp2 = scan.nextInt() - 1;
-                    System.out.println("Target CardStack? 1-7 ");
-                    tmp3 = scan.nextInt() - 1;
-                    if(gm1.canMoveToStack(tmp3, tmp, tmp2))
+                    tmp = getStackIndex();
+                    tmp2 = getCardIndex();
+                    tmp3 = getStackIndex();
+                    if(tmp >= 0 && tmp2 >= 0 && tmp3 >= 0 &&gm1.canMoveToStack(tmp3, tmp, tmp2))
                         gm1.moveToStack(tmp3, tmp, tmp2);
                     else
                         System.out.println(false);
                     break;
                 case 6:
-                    System.out.println("CardStack? 1-7 ");
-                    tmp = scan.nextInt() - 1;
-                    System.out.println("Index (from top, starting with 1)? ");
-                    tmp2 = scan.nextInt() - 1;
-                    System.out.println("FoundationPile? 1-4 ");
-                    tmp3 = scan.nextInt() - 1;
-                    if(gm1.canMoveToFoundation(tmp3, tmp, tmp2))
+                    tmp = getStackIndex();
+                    tmp2 = getCardIndex();
+                    tmp3 = getFoundationIndex();
+                    if(tmp >= 0 && tmp2 >= 0 && tmp3 >= 0 && gm1.canMoveToFoundation(tmp3, tmp, tmp2))
                         gm1.moveToFoundation(tmp3, tmp, tmp2);
                     else
                         System.out.println(false);
                     break;
-                case 7:System.out.println("From FoundationPile? 1-4 ");
-                    tmp = scan.nextInt() - 1;
-                    System.out.println("To FoundationPile? 1-4 ");
-                    if(gm1.canMoveToFoundation((tmp2 = scan.nextInt()-1), tmp))
+                case 7:
+                    tmp = getFoundationIndex();
+                    if(tmp >= 0 && (tmp2 = getFoundationIndex()) >= 0 && gm1.canMoveToFoundation(tmp2, tmp))
                         gm1.moveToFoundation(tmp2, tmp);
                     else
                         System.out.println(false);
@@ -85,4 +74,32 @@ public class MainCLI {
             System.out.println(menu);
         }
     }
+
+    public static int getFoundationIndex(){
+        System.out.println("FoundationPile? 1-4 ");
+        int x = scan.nextInt() - 1;
+        if(x >= 0 && x < 4)
+            return x;
+        else
+            return -1;
+    }
+
+    public static int getStackIndex(){
+        System.out.println("CardStack? 1-7 ");
+        int x = scan.nextInt() - 1;
+        if(x >= 0 && x < 7)
+            return x;
+        else
+            return -1;
+    }
+
+    public static int getCardIndex(){
+        System.out.println("Card? (from top, starting with 1) ");
+        int x = scan.nextInt() - 1;
+        if(x >= 0)
+            return x;
+        else
+            return -1;
+    }
+
 }
